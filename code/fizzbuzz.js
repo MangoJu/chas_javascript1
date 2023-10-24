7299b3526842d121f0b17dc544bc1a41a5808bed
Alma Angelica H <waddupmango@hotmail.com>
2023-10-24 17:53:18 +0200

    Update fizzbuzz.js

--- code/fizzbuzz.js
+++ code/fizzbuzz.js
@@ -5,40 +5,55 @@
 
 
 //solution 1
-for (let i = 1; i <= 100; i++) {
-if(i % 3 === 0 && i % 5 === 0) {
-        console.log("FizzBuzz");
+// for (let i = 1; i <= 100; i++) {
+// if(i % 3 === 0 && i % 5 === 0) {
+//         console.log("FizzBuzz");
     
-    } else if(i % 3 === 0){
-        console.log("Fizz");
+//     } else if(i % 3 === 0){
+//         console.log("Fizz");
             
-    }else if(i % 5 === 0){
-     console.log("Buzz");
+//     }else if(i % 5 === 0){
+//      console.log("Buzz");
     
+//     } else{
+//      console.log(i);
+//       }
+// }
+
+//solution 1.5
+for (let i = 1; i <= 100; i++) {
+    if(i % 3 === 0) {
+        if(i % 5 === 0){
+            console.log("FizzBuzz");
+            return
+        }
+        console.log("Fizz");
+    } else if(i % 5 === 0){
+        console.log("Buzz");  
     } else{
-     console.log(i);
-      }
-       }
+         console.log(i);
+          }
+    }
 
     
 //solution 2 med förklaringar av koden 
 
 // Loop from 1 to 100
-for (let i = 1; i <= 100; i++) {
-    // Check if the number is a multiple of both 3 and 5
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log('FizzBuzz');
-    }
-    // Check if the number is a multiple of 3
-    else if (i % 3 === 0) {
-        console.log('Fizz');
-    }
-    // Check if the number is a multiple of 5
-    else if (i % 5 === 0) {
-        console.log('Buzz');
-    }
-    // If none of the above conditions are met, just print the number
-    else {
-        console.log(i);
-    }
-}
+// for (let i = 1; i <= 100; i++) {
+//     // Check if the number is a multiple of both 3 and 5
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log('FizzBuzz');
+//     }
+//     // Check if the number is a multiple of 3
+//     else if (i % 3 === 0) {
+//         console.log('Fizz');
+//     }
+//     // Check if the number is a multiple of 5
+//     else if (i % 5 === 0) {
+//         console.log('Buzz');
+//     }
+//     // If none of the above conditions are met, just print the number
+//     else {
+//         console.log(i);
+//     }
+// }
